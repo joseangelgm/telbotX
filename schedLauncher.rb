ab8637bc856ea0b991c3ee486099edb58eb1f380
joseangelgm <jose.angel.garmon@gmail.com>
2020-03-27 16:09:43 +0100

    Added sched

---
+++ schedLauncher.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH << __dir__ + '/src/'
+
+STDIN.close # PIPE: we only will write through the pipe, just in case
+
+require 'modules/fileUtils'
+require 'modules/logger'
+
+include Logger
+include FileUtils
+
+# nedeed libaries and code
+begin
+    require 'yaml'
+    require 'sched'
+    Logger::log_message :info, "Libraries and code needed for sched were required properly..."
+rescue Exception => e
+    Logger::log_message :error, "Libraries needed for sched could not be found...Sutting down:", e
+    exit 1
+end
+
+CONFIG_FOLDER = "#{__dir__}/src/config/"
+SCHED_CONFIG  = "#{CONFIG_FOLDER}sched.yaml"
+
+sched_config = FileUtils::load_from_file SCHED_CONFIG
+Logger::log_message :info, "Config for sched", sched_config
+
+num_retry = 1
+launched  = false
+exception = nil
+sched     = Sched.new(sched_config[:ip], sched_config[:port])
+while !launched && num_retry <= sched_config[:retries]
+    begin
+        Logger::log_message :info, "Attemp #{num_retry} trying launch sched"
+        sched.create_socket
+        launched = true
+    rescue Exception => e
+        num_retry += 1
+        exception = e
+        sleep sched_config[:time_to_wait]
+    end
+end
+
+if !launched
+    Logger::log_message :error, "Sched could not be launched", exception
+    STDOUT.puts "{:exit => 1}"
+    STDOUT.close
+    exit 1
+end
+
+STDOUT.puts "{:exit => 0}"
+STDOUT.close
+thread_update = Thread.new { sched.run }
+thread_update.join # wait until thread_update finish
--- src/sched.rb
+++ src/sched.rb
@@ -1,2 +1,24 @@
+require 'sockets/serverTCP'
+
+require 'modules/logger'
+
 class Sched
+
+    include Logger
+
+    def initialize(ip, port)
+        @ip     = ip
+        @port   = port
+        @socket = nil
+    end
+
+    def create_socket
+        @socket = ServerTCP.new @ip, @port
+        log_message(:info, "Sched create server socket on #{@socket.ip} ip, port #{@socket.port}")
+    end
+
+    def run
+        @socket.close
+    end
+
 end
