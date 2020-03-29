9a9be0b5d7b3ed3a4d17d169a0296eddd070ec92
joseangelgm <jose.angel.garmon@gmail.com>
2020-03-29 20:05:21 +0200

    Added mutex

--- src/sched.rb
+++ src/sched.rb
@@ -10,19 +10,17 @@ class Sched
         @ip     = ip
         @port   = port
         @socket = nil
+
         @poweroff = false
         @mutex_poweroff = Mutex.new
+
+        @tasks = {}
+        @mutex_tasks = Mutex.new
     end
 
     def create_socket
         @socket = ServerTCP.new @ip, @port
         log_message(:info, "Sched create server socket on #{@socket.ip} ip, port #{@socket.port}")
-    end
-
-    def poweroff_socket
-        @mutex_poweroff.synchronize do
-            @poweroff = true if !@poweroff
-        end
     end
 
     def poweroff_state
@@ -35,9 +33,19 @@ class Sched
         client = @socket.accept #just one client
         while !poweroff_state do
             message = @socket.read_message client
-            log_message(:info, "Sched: receive command #{message}")
+            if message == "BYE"
+                poweroff_socket
+            end
+            log_message :info, "Sched: receive command #{message}"
         end
         @socket.close
+        log_message :info, "Sched socket closed"
     end
 
+    private
+    def poweroff_socket
+        @mutex_poweroff.synchronize do
+            @poweroff = true if !@poweroff
+        end
+    end
 end
