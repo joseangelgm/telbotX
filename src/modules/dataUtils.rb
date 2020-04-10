40a78cd6124a3312544fe791c1ec2488ae18bb62
joseangelgm <jose.angel.garmon@gmail.com>
2020-04-10 16:43:03 +0200

    Remove method

--- src/modules/dataUtils.rb
+++ src/modules/dataUtils.rb
@@ -7,22 +7,4 @@ module DataUtils
     def eval_to_array(string)
         return string.split
     end
-
-    #return filter a telegram message to fields that we want
-    def filter_telegram_message(hashmap)
-        message = {
-            :update_id => hashmap[:update_id],
-            :message   => {
-                :message_id => hashmap[:message][:message_id],
-                :first_name => hashmap[:message][:from][:first_name],
-                :chat_id    => hashmap[:message][:chat][:id],
-                :text       => hashmap[:message][:text]
-            }
-        }
-
-        time = Time.at(hashmap[:message][:date])
-        message[:message][:date] = "#{time.day}/#{time.month}/#{time.day} #{time.hour}:#{time.min}:#{time.sec}"
-        message
-    end
-
 end
