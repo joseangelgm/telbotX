cacb5e41d059ede3c9e31bfc9b153d7c303242ed
joseangelgm <jose.angel.garmon@gmail.com>
2020-04-10 16:44:52 +0200

    Start algorithm

--- src/sched.rb
+++ src/sched.rb
@@ -11,41 +11,6 @@ class Sched
         @port   = port
         @socket = nil
 
-        @poweroff = false
-        @mutex_poweroff = Mutex.new
-
-        @tasks = {}
-        @mutex_tasks = Mutex.new
     end
 
-    def create_socket
-        @socket = ServerTCP.new @ip, @port
-        log_message(:info, "Sched create server socket on #{@socket.ip} ip, port #{@socket.port}")
-    end
-
-    def poweroff_state
-        @mutex_poweroff.synchronize do
-            return @poweroff
-        end
-    end
-
-    def run
-        client = @socket.accept #just one client
-        while !poweroff_state do
-            message = @socket.read_message client
-            if message == "BYE"
-                poweroff_socket
-            end
-            log_message :info, "Sched: receive command #{message}"
-        end
-        @socket.close
-        log_message :info, "Sched socket closed"
-    end
-
-    private
-    def poweroff_socket
-        @mutex_poweroff.synchronize do
-            @poweroff = true if !@poweroff
-        end
-    end
 end
--- src/updater.rb
+++ src/updater.rb
@@ -14,73 +14,15 @@ class Updater
     GET_UPDATES = "getUpdates"
     private_constant :GET_UPDATES
 
+    attr_reader :update_id
+
     public
 
-    def initialize(bot_url, ip, port)
+    def initialize(bot_url, ip, port, update_id)
         @bot_url  = "#{bot_url}/#{GET_UPDATES}"
 
-        @ip       = ip
-        @port     = port
+        @ip   = ip
+        @port = port
 
-        @poweroff = false
-        @mutex_poweroff = Mutex.new
-
-        @tasks = {}
-        @mutex_tasks = Mutex.new
     end
-
-
-    def create_socket
-        @socket  = ClientTCP.new @ip, @port
-        log_message :info, "Updater create socket on #{@socket.ip} ip, port #{@socket.port}"
-    end
-
-    def poweroff_socket
-        @mutex_poweroff.synchronize do
-            @poweroff = true
-        end
-    end
-
-    def run
-        while !poweroff_state do
-            commands = get_updates
-            commands.each do |command|
-                command_parsed = filter_telegram_message command
-                log_message :info, "Sending command:", command_parsed
-                @socket.send_message command[:message][:text]
-                sleep 2
-            end
-            sleep 20
-        end
-        @socket.send_message "BYE"
-        @socket.close
-        log_message :info, "Poweroff updater..."
-    end
-
-    private
-
-    def poweroff_state
-        poweroff = false
-        @mutex_poweroff.synchronize do
-            poweroff = @poweroff
-        end
-        poweroff
-    end
-
-    def get_updates
-        response = make_query(@bot_url, :post, {})
-        message = {
-            :status      => response[:status],
-            :telegram_ok => response[:body][:ok],
-        }
-        ok = response[:body][:ok]
-        commands = []
-        if ok
-            commands = response[:body][:result]
-        end
-        message[:num_commands] = commands.count
-        log_message :info, "Command #{GET_UPDATES}", message
-        commands
-    end
-
 end
